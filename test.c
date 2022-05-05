8b044e6c8d444e3151edcd77e855770f2dcd5e2a
AKI VISHNU <akiyuvishnu@gmail.com>
2022-05-05 12:31:52 +0530

    commit

---
+++ test.c
@@ -0,0 +1,28 @@
+#include "unity.h"
+#include "ascii.h"
+#include<stdio.h>
+
+void setup()
+{
+
+}
+
+void tearDown()
+{
+
+}
+
+void test_sum_of_ascii_charectors(void)
+{
+
+    
+}
+
+
+int loop()
+{
+    UNITY_BEGIN();
+    RUN_TEST (sum_of_ascii_charectors);
+
+    UNITY_END();
+}
